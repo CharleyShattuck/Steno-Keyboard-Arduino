b00fae44dcddc600e54cc2920ea2cbb7ef6cd92f
CharleyShattuck <cshattuck@surewest.net>
2014-07-03 13:24:44 -0700

    Better naming of arrays, more self documenting.

--- mux_steno.ino
+++ mux_steno.ino
@@ -4,17 +4,17 @@
 boolean pressed;
 
 #define NO_BYTES 4
-byte b[NO_BYTES];
+byte data[NO_BYTES];
 
 #define NO_ROWS 4
-const byte r[]={9, 10, 11, 8};
+const byte row[]={9, 10, 11, 8};
 
 #define NO_COLS 6
-const byte c[]={2, 3, 4, 5, 6, 7};
+const byte column[]={2, 3, 4, 5, 6, 7};
 
 void setup() {
-  for(int i=0; i<NO_COLS; i++)  pinMode(c[i], INPUT_PULLUP);
-  for(int i=0; i<NO_ROWS; i++)  pinMode(r[i], INPUT);
+  for(int i=0; i<NO_COLS; i++)  pinMode(column[i], INPUT_PULLUP);
+  for(int i=0; i<NO_ROWS; i++)  pinMode(row[i], INPUT);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   Serial.begin(9600);
@@ -22,20 +22,20 @@ void setup() {
 }
 
 void loop() {
-  for(int i=0; i<NO_BYTES; i++)  b[i] = i * 0x40;
+  for(int i=0; i<NO_BYTES; i++)  data[i] = i * 0x40;
   do {scan();} while(!pressed);
   delay(5); // Debounce the first key pressed for 5 ms
   do {
     do {scan();} while(pressed);
     delay(10);
   } while(pressed); // Debounce the last release
-  for(int i=0; i<NO_BYTES; i++)  Serial.write(b[i]);
+  for(int i=0; i<NO_BYTES; i++)  Serial.write(data[i]);
 }
 
 void scan() {
   digitalWrite(13, HIGH); // toggle LED to time the scan
   pressed = false;
-  for(int i=0; i<NO_BYTES; i++)  b[i] |= getRow(r[i]);
+  for(int i=0; i<NO_BYTES; i++)  data[i] |= getRow(row[i]);
   digitalWrite(13, LOW);
 }  
 
@@ -44,7 +44,7 @@ byte getRow(int pin) {
   pinMode(pin, OUTPUT);
   digitalWrite(pin, LOW);
   byte row = 0;
-  for(int i=0; i<NO_COLS; i++)  row |= pinState(c[i]) << i;
+  for(int i=0; i<NO_COLS; i++)  row |= pinState(column[i]) << i;
   pinMode(pin, INPUT);
   return row;
 }
