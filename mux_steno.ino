c0eb9c311bfcc26fdaf7eab03e81a224de30f31a
CharleyShattuck <cshattuck@surewest.net>
2014-07-02 17:53:49 -0700

    Simplified the program a bit with fewer parameters passed and more local variables. Eliminated a function or two as well as the commented out debugging code. It still works.

--- mux_steno.ino
+++ mux_steno.ino
@@ -11,11 +11,8 @@
 #define c3 5
 #define c4 6
 #define c5 7
-byte b0 = 0;
-byte b1 = 0x40;
-byte b2 = 0x80;
-byte b3 = 0xc0;
-boolean pressed = false;
+byte b0, b1, b2, b3;
+boolean pressed;
 
 void setup() {
   for (int i=2; i<8; i++) pinMode(i, INPUT_PULLUP);
@@ -27,80 +24,48 @@ void setup() {
 }
 
 void loop() {
-// Used to probe and verify the wiring
-//  pinMode(O4, OUTPUT);
-//  digitalWrite(O4, LOW);
-//  int x = digitalRead(6);
-//  if (x == HIGH) {
-//    digitalWrite(13, HIGH);
-//  } else {
-//    digitalWrite(13, LOW);
-//  }
+  b0 = 0;
+  b1 = 0x40;
+  b2 = 0x80;
+  b3 = 0xc0;
   do {scan();} while(!pressed);
   delay(5); // Debounce the first key pressed for 5 ms
   do {
     do {scan();} while(pressed);
     delay(10);
-  }
-  while(pressed); // Debounce the last release
-  TXBolt(); // Serial could take 4 ms minimum, add 1 more delay to debounce
-//  scan(); // debug scan without pressed
-//  show();
+  } while(pressed); // Debounce the last release
+  Serial.write(b0);
+  Serial.write(b1);
+  Serial.write(b2);
+  Serial.write(b3);
 }
 
 void scan() {
   digitalWrite(13, HIGH); // toggle LED to time the scan
   pressed = false;
-  b0 = getRow(r0, b0);
-  b1 = getRow(r1, b1);
-  b2 = getRow(r2, b2);
-  b3 = getRow(r3, b3);
+  b0 |= getRow(r0);
+  b1 |= getRow(r1);
+  b2 |= getRow(r2);
+  b3 |= getRow(r3);
   digitalWrite(13, LOW);
 }  
 
 // Activate and read one row  
-byte getRow(int r, byte row) {
-  pinMode(r, OUTPUT);
-  digitalWrite(r, LOW);
-  row = getKey(c0, 0x01, row);
-  row = getKey(c1, 0x02, row);
-  row = getKey(c2, 0x04, row);
-  row = getKey(c3, 0x08, row);
-  row = getKey(c4, 0x10, row);
-  row = getKey(c5, 0x20, row);
-  pinMode(r, INPUT);
+byte getRow(int pin) {
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, LOW);
+  byte row = pinState(c0);
+  row |= pinState(c1) << 1;
+  row |= pinState(c2) << 2;
+  row |= pinState(c3) << 3;
+  row |= pinState(c4) << 4;
+  row |= pinState(c5) << 5;
+  pinMode(pin, INPUT);
   return row;
 }
 
-// Read and record one key into its byte
-byte getKey(int k, byte b, byte v) {
-  if (digitalRead(k) == 0) {
-    v |= b;
-    pressed = true;
-  }
-  return v;
-}
-
-// TX Bolt protocol, simplified, unconditional
-void TXBolt() {
-  Serial.write(b0);
-  Serial.write(b1);
-  Serial.write(b2);
-  Serial.write(b3);
-  b0 = 0;
-  b1 = 0x40;
-  b2 = 0x80;
-  b3 = 0xc0;
-} 
-
-// Debug display, no TX protocol
-void show() {
-  Serial.print(b0, HEX); Serial.print(" ");
-  Serial.print(b1, HEX); Serial.print(" ");
-  Serial.print(b2, HEX); Serial.print(" ");
-  Serial.print(b3, HEX); Serial.print("\n");
-  b0 = 0;
-  b1 = 0x40;
-  b2 = 0x80;
-  b3 = 0xc0;
+byte pinState(int pin) {
+  byte state = !digitalRead(pin);
+  if(state == 1) pressed = true;
+  return state;
 }
