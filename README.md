d541b01e84f01b347c03eea769d224600aa0f76c
CharleyShattuck <cshattuck@surewest.net>
2015-01-30 11:32:26 -0800

    README.md

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Steno-Keyboard-Arduino
 ======================
 
 Arduino code for a homemade steno keyboard.
+
+steno_keyboard.ino
+   The original, separate pin for each key. Arduino Mega.
+
+mux_steno.ino
+   Second try, multiplexed. Arduino Micro.
+
+Interpreter.ino
+   Unrelated project. A Forth style interpreter.
--- steno_keyboard.ino
+++ steno_keyboard.ino
@@ -40,32 +40,33 @@ void loop() {
 void scan() {
   pressed = false;
 
-  val0 = getKey(36, 1, val0);
-  val0 = getKey(31, 2, val0);
-  val0 = getKey(30, 4, val0);
-  val0 = getKey(33, 8, val0);
-  val0 = getKey(32, 16, val0);
-  val0 = getKey(35, 32, val0); 
+//             pin, mask, byte
+  val0 = getKey(36, 1, val0); /* S */
+  val0 = getKey(31, 2, val0); /* T */
+  val0 = getKey(30, 4, val0); /* K */
+  val0 = getKey(33, 8, val0); /* P */
+  val0 = getKey(32, 16, val0);   /* W */
+  val0 = getKey(35, 32, val0);   /* H */
 
-  val1 = getKey(34, 1, val1);
-  val1 = getKey(52, 2, val1);
-  val1 = getKey(53, 4, val1);
-  val1 = getKey(50, 8, val1);
-  val1 = getKey(51, 16, val1);
-  val1 = getKey(48, 32, val1); 
+  val1 = getKey(34, 1, val1); /* R */
+  val1 = getKey(52, 2, val1); /* A */
+  val1 = getKey(53, 4, val1); /* O */
+  val1 = getKey(50, 8, val1); /* * */
+  val1 = getKey(51, 16, val1);   /* E */
+  val1 = getKey(48, 32, val1);   /* U */
  
-  val2 = getKey(47, 1, val2);
-  val2 = getKey(45, 2, val2);
-  val2 = getKey(49, 4, val2);
-  val2 = getKey(46, 8, val2);
-  val2 = getKey(43, 16, val2);
-  val2 = getKey(44, 32, val2); 
+  val2 = getKey(47, 1, val2); /* F */
+  val2 = getKey(45, 2, val2); /* R */
+  val2 = getKey(49, 4, val2); /* P */
+  val2 = getKey(46, 8, val2); /* B */
+  val2 = getKey(43, 16, val2);   /* L */
+  val2 = getKey(44, 32, val2);   /* G */
 
-  val3 = getKey(41, 1, val3);
-  val3 = getKey(42, 2, val3);
-  val3 = getKey(39, 4, val3);
-  val3 = getKey(40, 8, val3);
-  val3 = getKey(38, 16, val3);
+  val3 = getKey(41, 1, val3); /* T */
+  val3 = getKey(42, 2, val3); /* S */
+  val3 = getKey(39, 4, val3); /* D */
+  val3 = getKey(40, 8, val3); /* Z */
+  val3 = getKey(38, 16, val3);   /* # */
 } 
 
 // k = Arduino pin number
