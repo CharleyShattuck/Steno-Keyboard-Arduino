ae9446640836532caf8b4ca8c94faa4356fabda3
Charley Shattuck <cshattuck@surewest.net>
2018-09-05 22:44:58 -0700

    Modified mux_steno for Adafruit Itsy Bitsy NKRO
    Uses a jumper from pin 8 to ground to set NKRO keyboard protocol

--- mux_steno.ino
+++ mux_steno.ino
@@ -1,56 +1,143 @@
+#include <Keyboard.h>
+
 // Multiplexed Steno Keyboard
 // This example program is in the public domain
+// modified August 5, 2018 to make row pins HIGH
+// for the diodes.
+// modified August 18-19 for NKRO protocol
 
 boolean pressed;
+boolean first;
 
+// four bytes for the TX Bolt protocol
 #define NO_BYTES 4
 byte data[NO_BYTES];
 
+// matrix wiring
 #define NO_ROWS 4
-const byte row[]={9, 10, 11, 8};
-
+const byte row[]={9, 10, 11, 12};
 #define NO_COLS 6
-const byte column[]={2, 3, 4, 5, 6, 7};
+const byte column[]={A0, A1, A2, A3, A4, A5};
 
 void setup() {
   for(int i=0; i<NO_COLS; i++)  pinMode(column[i], INPUT_PULLUP);
-  for(int i=0; i<NO_ROWS; i++)  pinMode(row[i], INPUT);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  for(int i=0; i<NO_ROWS; i++){
+    pinMode(row[i], OUTPUT); digitalWrite(row[i], HIGH);
+  }
+  pinMode(8, INPUT_PULLUP);
+  pinMode(13, OUTPUT); digitalWrite(13, HIGH);
+  if(digitalRead(8)){
+    TX_Bolt(); // no shunt installed
+  }else{
+    NKRO_keyboard(); // shunt is installed
+  }
+}
+
+// not using "loop" but Arduino IDE wants it to exist
+void loop(){
+  while(true);
+}
+
+// NKRO protocol
+
+void spit(char a){
+  Keyboard.press(a); delay(2);
+  if(first == true){
+    first = false;
+  } else{
+    Keyboard.release(a);
+  }
+}
+
+void sendNKRO(){
+  first = true;
+  
+  if (data[0] & 0x01)spit('q');
+  if (data[0] & 0x02)spit('w');
+  if (data[0] & 0x04)spit('s');
+  if (data[0] & 0x08)spit('e');
+  if (data[0] & 0x10)spit('d');
+  if (data[0] & 0x20)spit('r');
+   
+  if (data[1] & 0x01)spit('f');  
+  if (data[1] & 0x02)spit('c');  
+  if (data[1] & 0x04)spit('v');  
+  if (data[1] & 0x08)spit('t');
+  if (data[1] & 0x10)spit('n');
+  if (data[1] & 0x20)spit('m');
+
+  if (data[2] & 0x01)spit('u');  
+  if (data[2] & 0x02)spit('j');  
+  if (data[2] & 0x04)spit('i');  
+  if (data[2] & 0x08)spit('k');
+  if (data[2] & 0x10)spit('o');
+  if (data[2] & 0x20)spit('l');
+
+  if (data[3] & 0x01)spit('p');  
+  if (data[3] & 0x02)spit(';');  
+  if (data[3] & 0x04)spit('[');  
+  if (data[3] & 0x08)spit('\'');
+  if (data[3] & 0x10)spit('3');
+
+  Keyboard.releaseAll();
+}
+
+void NKRO_keyboard(){
+  Keyboard.begin();
+  delay(3000);
+  while(true){
+    scan();
+    sendNKRO();
+  }
+}
+
+// TX Bolt protocol
+
+void sendTX(){
+  for(int i=0; i<NO_BYTES; i++) Serial.write(data[i]);
+  delay(20);  // wait a bit before scanning again    
+}
+
+void TX_Bolt(){
   Serial.begin(9600);
   delay(3000); // Apparently Arduino Micro needs this
+  while(true){
+    scan();
+    sendTX();
+  }
 }
 
-void loop() {
-  for(int i=0; i<NO_BYTES; i++)  data[i] = i * 0x40;
-  do {scan();} while(!pressed);
-  delay(5); // Debounce the first key pressed for 5 ms
-  do {
-    do {scan();} while(pressed);
-    delay(10);
-  } while(pressed); // Debounce the last release
-  for(int i=0; i<NO_BYTES; i++)  Serial.write(data[i]);
-}
-
-void scan() {
-  digitalWrite(13, HIGH); // toggle LED to time the scan
-  pressed = false;
-  for(int i=0; i<NO_BYTES; i++)  data[i] |= getRow(row[i]);
-  digitalWrite(13, LOW);
-}  
-
-// Activate and read one row  
-byte getRow(int pin) {
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, LOW);
-  byte row = 0;
-  for(int i=0; i<NO_COLS; i++)  row |= pinState(column[i]) << i;
-  pinMode(pin, INPUT);
-  return row;
-}
+// scan for keypresses
 
 byte pinState(int pin) {
   byte state = !digitalRead(pin);
   if(state == 1)  pressed = true;
   return state;
 }
+
+// Activate and read one row  
+byte getRow(int pin) {
+  digitalWrite(pin, LOW);
+  byte row = 0;
+  for(int i=0; i<NO_COLS; i++)  row |= pinState(column[i]) << i;
+  digitalWrite(pin, HIGH);
+  return row;
+}
+
+// look at each key once
+void look() {
+  pressed = false;
+  for(int i=0; i<NO_BYTES; i++)  data[i] |= getRow(row[i]);
+}  
+
+// wait for keypress and scan until all released
+void scan(){
+  do {
+    for(int i=0; i<NO_BYTES; i++) data[i] = i * 0x40; // zero data
+    do {look();} while(!pressed); delay(20);
+  } while(!pressed);
+  digitalWrite(13, HIGH);
+  do {look();} while(pressed);
+  digitalWrite(13, LOW);
+}
+
