4f16581848d3ba7cf6d9c96b10fe87fefc445ceb
Charley Shattuck <cshattuck@surewest.net>
2019-03-13 22:26:55 -0700

    Beginning to add some primitives

--- M0-Forth.ino
+++ M0-Forth.ino
@@ -19,17 +19,22 @@ int W = 0; // working register
 
 // function prototypes for the primitives
 void _dummy (void);
-void _docode (void);
-void _docolon (void);
-void _doconst (void);
+void _const (void);
+void _code (void);
+void _enter (void);
+void _lit (void);
+void _exit (void);
+void _key (void);
+void _emit (void);
 void _dup (void);
 void _drop (void);
 void _swap (void);
+void _1plus (void);
 
 // primitive function array
 void (*primitive []) (void) = {
-  _docode, _dup, _drop, _swap, _dummy, _dummy, _dummy, _dummy,
-  _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy,
+  _lit, _code, _enter, _exit, _key, _emit, _dup, _drop,
+  _swap, _1plus, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy,
   _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy,
   _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy,
   _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy, _dummy,
@@ -43,21 +48,52 @@ void _dummy (void) {
   Serial.print ("undefined");  
 }
 
-void _docode (void) {
+void _code (void) {
   W = memory [I++];
   primitive [W] ();
 }
 
+void _lit (void) {
+  int T = memory [I++];
+  memory [--S] = T;
+}
+
+void _enter (void) {
+
+}
+
+void _exit (void) {
+
+}
+
+void _key (void) {
+
+}
+
+void _emit (void) {
+  char c = memory [S++];
+  Serial.write (c);
+}
+
 void _dup (void) {
-  Serial.print (" dup ");
+  int T = memory [S--];
+  memory [S] = T;
 }
 
 void _drop (void) {
-  Serial.print (" drop ");
+  S += 1;
 }
 
 void _swap (void) {
-  Serial.print (" swap ");
+  int T = memory [S++];
+  int N = memory [S];
+  memory [S--] = T;
+  memory [S] = N;
+}
+
+void _1plus (void) {
+  int T = (memory [S] + 1);
+  memory [S] = T;
 }
 
 // the setup function runs once when you press reset or power the board
@@ -66,13 +102,15 @@ void setup() {
   S = S0;
   R = R0;
   Serial.begin (9600);
+// test
+  memory [--S] = 0x41; // push 'A' onto the stack
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  delay (5000);
 // test
-  W = 1; primitive [W] ();
-  W = 2; primitive [W] ();
-  W = 3; primitive [W] ();
+  delay (1000);
+  W = 6; primitive [W] (); // execute "dup"
+  W = 5; primitive [W] (); // execute "emit"
+  W = 9; primitive [W] (); // execute "1+"
 }
