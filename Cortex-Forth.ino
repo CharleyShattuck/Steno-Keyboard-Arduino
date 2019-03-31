1e1e70dbbc39da9aac84dd2e7013f2968456365b
Charley Shattuck <cshattuck@surewest.net>
2019-03-30 22:28:11 -0700

    Quit loop works now, except for compiling

--- Cortex-Forth.ino
+++ Cortex-Forth.ino
@@ -11,7 +11,7 @@
 #define NAME(m, f, c, x, y, z) {memory.data [m] = f + c + (x << 8) + (y << 16) + (z << 24);}
 #define LINK(m, a) {memory.data [m] = a;}
 #define CODE(m, a) {memory.program [m] = a;}
-#define LIST(m, a) {memory.data [m] = a;}
+#define DATA(m, a) {memory.data [m] = a;}
 
 // global variables
 union Memory {
@@ -30,11 +30,11 @@ int D = 0; // dictionary list entry point
 int base = 10;
 
 /*  A word in the dictionary has these fields:
-  link  32b  point to next word in list, 0 says end of list
-  name  32b  a 32 bit int, made up of byte count and three letters
-  code  32b  a pointer to some actually C compiled code,
+  name  32b word,  a 32 bit int, made up of byte count and three letters
+  link  32b word, point to next word in list, 0 says end of list
+  code  32b word, a pointer to some actual C compiled code,
         all native code is in this field
-  data  32b at least  a list to execute or a data field of some kind
+  data  32b word, at least, a list to execute or a data field of some kind
 
 
 */
@@ -91,7 +91,7 @@ void _SWAP (void) {
 
 void _OVER (void) {
   _DUP ();
-  T = memory.data [S - 1];
+  T = memory.data [S + 1];
 }
 
 void _FETCH (void) {
@@ -160,10 +160,8 @@ void _TWOSTAR (void) {
   T = (T << 1);
 }
 
-// unlinked primitives
-
 void _LIT (void) {
-  memory.data [--S] = T;
+  _DUP (); 
   T = memory.data [I++];
 }
 
@@ -264,7 +262,8 @@ void _NUMBER (void) {
     t = tib [i];
     if (!isDigit (t)) {
       if (tib [0] == '-') T = -T;
-      memory.data [--S] = -1;
+      _DUP ();
+      T = -1;
       return;
     }
     T *= base;
@@ -273,13 +272,10 @@ void _NUMBER (void) {
     T += t;
   }
   if (tib [0] == '-') T = -T;
-  memory.data [--S] = 0;
+  _DUP ();
+  T = 0;
 }
 
-// void _EXECUTE (void) {
-//   memory.data [--R] = I;
-//   I = (T + 2);
-// }
 void _EXECUTE (void) {
   W = (T + 2);
   _DROP ();
@@ -316,8 +312,8 @@ void _DDOTS (void) {
     return;
   }
   _DUP ();
-  W = S0;
-  while (W > (S + 1)) {
+  W = (S0 - 1);
+  while (W > (S)) {
     Serial.print (memory.data [--W]);
     Serial.write (' ');
   }
@@ -371,7 +367,7 @@ void _WORDS (void) {
     W = memory.data [++W];
   }
   _CR ();
-  _DROP ();
+//  _DROP ();
 }
 
 void _DEPTH (void) {
@@ -509,93 +505,140 @@ void setup () {
 
   // abort
   NAME(80, 0, 5, 'a', 'b', 'o')
-  LINK(81, 80)
+  LINK(81, 77)
   CODE(82, _NEST)
-  LIST(83, inits)
+  DATA(83, inits)
 #  define abort 83
   // again
-  LIST(84, branch)
-  LIST(85, 89)
+  DATA(84, branch)
+  DATA(85, 89)
   // quit
   NAME(86, 0, 4, 'q', 'u', 'i')
   LINK(87, 77)
   CODE(88, _NEST)
-  LIST(89, initr)
+  DATA(89, initr)
   // begin quit loop
-  LIST(90, parse)
-  LIST(91, wword)
-  LIST(92, find)
-  LIST(93, qdup)
-  LIST(94, zbranch)
-  LIST(95, 101) // to number
-  LIST(96, execute)
-  LIST(97, depth)
-  LIST(98, zeroless)
-  LIST(99, zbranch)
-  LIST(100, 112) // to ok
-  LIST(101, number)
-  LIST(102, zbranch)
-  LIST(103, 112) // to ok
-  LIST(104, showtib)
-  LIST(105, lit)
-  LIST(106, '?')
-  LIST(107, emit)
-  LIST(108, cr)
-  LIST(109, inits)
-  LIST(110, branch)
-  LIST(111, abort)
-  LIST(112, ok)
-  LIST(113, branch)
-  LIST(114, 90) // continue quit loop
-
+  DATA(90, parse)
+  DATA(91, wword)
+  DATA(92, find)
+  DATA(93, qdup)
+  DATA(94, zbranch)
+  DATA(95, 103) // to number
+  DATA(96, execute)
+  DATA(97, depth)
+  DATA(98, zeroless)
+  DATA(99, zbranch)
+  DATA(100, 114) // to ok
+  DATA(101, branch)
+  DATA(102, 106)
+  DATA(103, number)
+  DATA(104, zbranch)
+  DATA(105, 114) // to ok
+  DATA(106, showtib)
+  DATA(107, lit)
+  DATA(108, '?')
+  DATA(109, emit)
+  DATA(110, cr)
+  DATA(111, inits)
+  DATA(112, branch)
+  DATA(113, 89)
+  DATA(114, ok)
+  DATA(115, branch)
+  DATA(116, 90) // continue quit loop
 
   // .
-  NAME(115, 0, 1, '.', 0, 0)
-  LINK(116, 86)
-  CODE(117, _DOT)
-#  define dot 117
+  NAME(117, 0, 1, '.', 0, 0)
+  LINK(118, 86)
+  CODE(119, _DOT)
+#  define dot 119
   // .s
-  NAME(118, 0, 2, '.', 's', 0)
-  LINK(119, 115)
-  CODE(120, _DDOTS)
-#  define ddots 120
+  NAME(120, 0, 2, '.', 's', 0)
+  LINK(121, 117)
+  CODE(122, _DDOTS)
+#  define ddots 122
   // words
-  NAME(121, 0, 5, 'w', 'o', 'r')
-  LINK(122, 118)
-  CODE(123, _WORDS)
-#  define words 123
+  NAME(123, 0, 5, 'w', 'o', 'r')
+  LINK(124, 120)
+  CODE(125, _WORDS)
+#  define words 125
   // space
-  NAME(124, 0, 5, 's', 'p', 'a')
-  LINK(125, 121)
-  CODE(126, _SPACE)
-#  define space 126
+  NAME(126, 0, 5, 's', 'p', 'a')
+  LINK(127, 123)
+  CODE(128, _SPACE)
+#  define space 128
   // h.
-  NAME(127, 0, 2, 'h', '.', 0)
-  LINK(128, 124)
-  CODE(129, _HDOT)
-#  define hdot 129
+  NAME(129, 0, 2, 'h', '.', 0)
+  LINK(130, 126)
+  CODE(131, _HDOT)
+#  define hdot 131
+  // +
+  NAME(132, 0, 1, '+', 0, 0)
+  LINK(133, 129)
+  CODE(134, _PLUS)
+  // -
+  NAME(135, 0, 1, '-', 0, 0)
+  LINK(136, 132)
+  CODE(137, _MINUS)
+  // and
+  NAME(138, 0, 3, 'a', 'n', 'd')
+  LINK(139, 135)
+  CODE(140, _aND)
+  // or
+  NAME(141, 0, 2, 'o', 'r', 0)
+  LINK(142, 138)
+  CODE(143, _OR)
+  // xor
+  NAME(144, 0, 3, 'x', 'o', 'r')
+  LINK(145, 141)
+  CODE(146, _XOR)
+  // invert
+  NAME(147, 0, 6, 'i', 'n', 'v')
+  LINK(148, 144)
+  CODE(149, _INVERT)
+  // abs
+  NAME(150, 0, 3, 'a', 'b', 's')
+  LINK(151, 147)
+  CODE(152, _ABS)
+  // negate
+  NAME(153, 0, 6, 'n', 'e', 'g')
+  LINK(154, 150)
+  CODE(155, _NEGATE)
+  // 2*
+  NAME(156, 0, 2, '2', '*', 0)
+  LINK(157, 153)
+  CODE(158, _TWOSTAR)
+  // 2/
+  NAME(159, 0, 2, '2', '/', 0)
+  LINK(160, 156)
+  CODE(161, _TWOSLASH)
+
+  D = 159; // latest word
+  H = 162; // top of dictionary
 
   // test
-  LIST(200, parse)
-  LIST(201, wword)
-  LIST(202, find)
-  LIST(203, execute)
-//  LIST(203, ddots)
-//  LIST(204, showtib)
-  LIST(204, branch)
-  LIST(205, 200)
+  DATA(200, parse)
+  DATA(201, wword)
+  DATA(202, find)
+  DATA(203, dot)
+  DATA(204, number)
+  DATA(205, dot)
+  DATA(206, dot)
+//  DATA(201, wword)
+//  DATA(202, find)
+//  DATA(203, execute)
+  DATA(207, ddots)
+  DATA(208, showtib)
+  DATA(209, branch)
+  DATA(210, 200)
 
-  D = 127; // latest word
-  H = 130; // top of dictionary
-
-  I = abort; // instruction pointer = abreort
+  I = abort; // instruction pointer = abort
 //  I = 200; //  test
   Serial.begin (9600);
   while (!Serial);
   Serial.println ("myForth Arm Cortex");
   //  _WORDS ();
   //  _DEPTH ();
-  _DDOTS ();
+  //  _DDOTS ();
 }
 
 // the loop function runs over and over again forever
@@ -604,49 +647,6 @@ void loop() {
   memory.program [W] ();
   //  delay (300);
 }
-
-/*
-  // +
-  NAME(49, 0, 1, '+', 0, 0)
-  LINK(50, 45)
-  CODE(51, _PLUS)
-  // -
-  NAME(53, 0, 1, '-', 0, 0)
-  LINK(54, 49)
-  CODE(55, _MINUS)
-  // and
-  NAME(57, 0, 3, 'a', 'n', 'd')
-  LINK(58, 53)
-  CODE(59, _aND)
-  // or
-  NAME(61, 0, 2, 'o', 'r', 0)
-  LINK(62, 57)
-  CODE(63, _OR)
-  // xor
-  NAME(65, 0, 3, 'x', 'o', 'r')
-  LINK(66, 61)
-  CODE(67, _XOR)
-  // invert
-  NAME(69, 0, 6, 'i', 'n', 'v')
-  LINK(70, 65)
-  CODE(71, _INVERT)
-  // abs
-  NAME(73, 0, 3, 'a', 'b', 's')
-  LINK(74, 69)
-  CODE(75, _ABS)
-  // negate
-  NAME(77, 0, 6, 'n', 'e', 'g')
-  LINK(78, 73)
-  CODE(79, _NEGATE)
-  // 2*
-  NAME(81, 0, 2, '2', '*', 0)
-  LINK(82, 77)
-  CODE(83, _TWOSTAR)
-  // 2/
-  NAME(85, 0, 2, '2', '/', 0)
-  LINK(86, 81)
-  CODE(87, _TWOSLASH)
-*/
 
 
 
