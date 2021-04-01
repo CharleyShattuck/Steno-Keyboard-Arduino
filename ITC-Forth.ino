79b2f846c017595dff82569b0de20be42b294182
Charley Shattuck <cshattuck@surewest.net>
2021-04-01 15:52:08 -0700

    simplest proof of concept

---
+++ ITC-Forth.ino
@@ -0,0 +1,49 @@
+// ITC-Forth.ino
+
+#define RAM_SIZE 0x1200
+#define S0 0x1000
+#define R0 0x0f00
+
+// "registers"
+int S = S0; // data stack pointer
+int R = R0; // return stack pointer
+int I = 0; // instruction pointer
+int W = 0; // working register
+
+const int memory [] {
+  1, 2, 3, 0,
+};
+
+void runForth () {
+next:
+  W = memory [I++];
+  switch (W) {
+    case 1:
+    A:
+      Serial.write ('A');
+      goto next;
+    case 2:
+    _delay:
+      delay (1000);
+      goto next;
+    case 3:
+    branch:
+      I = memory [I];
+      goto next;
+  }
+}
+
+void setup () {
+  Serial.begin (9600);
+  I = 0;
+  S = S0;
+  R = R0;
+  runForth ();
+}
+
+void loop () {
+  while (1) {
+//    Serial.write ('A');
+//    delay (1000); 
+  };
+}
